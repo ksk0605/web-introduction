fc191bc335ee429a7d13fd5dcdbade322d713024
SungKyum Kim <sungkyum1@naver.com>
2024-03-06 12:10:50 +0900

    영화 링크 추가

--- js/index.js
+++ js/index.js
@@ -4,28 +4,29 @@ const movie3 = document.getElementById("movie3");
 
 const registrationButton = document.getElementById("registration");
 const resetButton = document.getElementById("reset");
+const nameInput = document.getElementById("name");
 
 registrationButton.addEventListener("click", registrate);
 resetButton.addEventListener("click", reset);
 
 function registrate() {
     if (movie1.checked && movie2.checked && movie3.checked) {
-        alert("뭘 좀 아시는 군요~");
+        alert(nameInput.innerText + "뭘 좀 아시는 군요~");
     }
     else if (movie1.checked && movie2.checked) {
-        alert("저와 2개나 같네요~");
+        alert(nameInput.innerText + "저와 2개나 같네요~");
     }
     else if (movie1.checked && movie3.checked) {
-        alert("저와 2개나 같네요~");
+        alert(nameInput.innerText + "저와 2개나 같네요~");
     }
     else if (movie2.checked && movie3.checked) {
-        alert("저와 2개나 같네요~");
+        alert(nameInput.innerText + "저와 2개나 같네요~");
     }
     else if (movie1.checked || movie2.checked || movie3.checked ){
-        alert("저와 1개 밖에 안 같네요 ㅠ");
+        alert(nameInput.innerText + "저와 1개 밖에 안 같네요 ㅠ");
     }
     else {
-        alert("상처 받았어요 나가주세요");
+        alert(nameInput.innerText + "상처 받았어요 나가주세요");
     }
 }
 
@@ -33,4 +34,5 @@ function reset() {
     movie1.checked = false;
     movie2.checked = false;
     movie3.checked = false;
+    nameInput.innerText = '';
 }
